c8d3941161e9fcbcb520727b90e487d635b00db4
Sudarshan <sudarshantanwer@gmail.com>
2024-05-16 13:24:07 +0530

    Added express middleware

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.use((req, res, next)=>{
+     console.log("middleware one");
+     next();
+})
+
+app.use((req, res, next)=>{
+     console.log("middleware two");
+     next();
+})
+
 // Define a route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+
 
 // Start the server
 app.listen(port, () => {
